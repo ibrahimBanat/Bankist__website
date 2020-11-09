ae665ee191caa93c90ce3165e71710a97e06bccc
ibrahim banat <ibrahim.banat.97@gmail.com>
2020-11-09 19:35:50 +0200

    Adding smooth scroll

--- script.js
+++ script.js
@@ -7,8 +7,10 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const header = document.querySelector('.header');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +20,9 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+
+
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -28,4 +31,33 @@ document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
+});
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+
+message.innerHTML = 'We use cookied for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+header.append(message);
+
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', ()=> {
+    message.remove();
+  })
+
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+message.style.height = 
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+
+btnScrollTo.addEventListener('click', (e) => { 
+  const s1coords = section1.getBoundingClientRect();
+  //Scrolling 
+  section1.scrollIntoView({behavior:'smooth'});
 });
